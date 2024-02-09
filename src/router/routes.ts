fa62176a89a7c0bd3ab656a5b62dd689c76dfa6f
corentin lechêne <corentin.lechene.pro@gmail.com>
2024-02-09 19:29:23 +0100

    Initial commit

---
+++ src/router/routes.ts
@@ -0,0 +1,35 @@
+import {RouteRecordRaw} from "vue-router";
+import LoginPage from "@/views/LoginPage.vue"; //todo après ça sera profiles
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: "/",
+        redirect: "/login" //todo après ça sera profiles
+    },
+    {
+        path: "/login",
+        component: LoginPage
+    }
+    // {
+    //     path: "/register",
+    //     component: () => import("@/views/RegisterPage.vue")
+    // },
+    // {
+    //     path: "/onboarding",
+    //     component: () => import("@/views/Onboarding.vue")
+    // },
+    // {
+    //     path: "/profiles",
+    //     component: () => import("@/views/Profiles.vue")
+    // },
+    // {
+    //     path: "/galleries",
+    //     component: () => import("@/views/GalleriesPage.vue")
+    // },
+    //{
+    //     path: "/galleries/:galleryId",
+    //     component: () => import("@/views/GalleryDetail.vue")
+    // },
+];
+
+export default routes;
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import {ref} from 'vue'
+
+const checked = ref(false);
+
+</script>
+
+<template>
+  <div>
+    qsdqsd
+    <Button label="Submit" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
