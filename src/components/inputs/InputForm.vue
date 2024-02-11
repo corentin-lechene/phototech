25401c085ab7ae11992f4f1e4e8a80eced78ead6
melis <melissanoemi.laurent@free.fr>
2024-02-11 07:39:41 +0100

    fix pr

--- src/components/inputs/InputForm.vue
+++ src/components/inputs/InputForm.vue
@@ -12,9 +12,9 @@ const s = ref('');
 
 <template>
   <div class="card flex w-full">
-    <div class="flex flex-column gap-2 w-full mb-3">
+    <div class="flex flex-column gap-2 w-full mb-4">
       <InputText v-model="s" :placeholder=placeholder class="h-3rem"/>
-      <small v-if="messageError?.length != 0" class="align-self-start text-red-500">{{messageError}}</small>
+      <small v-if="messageError" class="align-self-start text-red-500">{{messageError}}</small>
     </div>
   </div>
 </template>
